4e1f4b3fd2a0c7bdc1cc25cccc21ccc7ee2b6967
papa28x4 <swissfair@yahoo.co.uk>
2020-11-28 17:34:05 +0100

    fine-tune date function

--- js/graphql.js
+++ js/graphql.js
@@ -64,11 +64,10 @@ const payload = `query
 }
   `
 
-
-const formatDate=(dateString)=>{
+  const formatDate=(dateString)=>{
     let date = "";
     let num = "";
-    // currentYear
+    let currentYr = new Date().getFullYear()
     let timestamp = new Date(dateString).getTime() 
     if(timestamp + (28*86400000) < Date.now()){
         let z = new Date(timestamp).toDateString()
@@ -78,7 +77,8 @@ const formatDate=(dateString)=>{
             if(i===1){
                 date += " "
             }else if(i===2){
-                date += ", "
+                if(currentYr === +t) return
+                date +=  ", "
             }
             date += t
         })
@@ -115,10 +115,12 @@ const renderRepos = repos => {
             ${repo.description? `  <p class="repo-desc">${repo.description}</p>` : ''}
         
             <div class="repo-meta">
-                <span class="repo-lang meta">
-                <span class="repo-lang-legend" style="background-color:${repo.primaryLanguage.color}"></span>
-                <span>${repo.primaryLanguage.name}</span>
-                </span>
+                
+                ${repo.primaryLanguage? `<span class="repo-lang meta">
+                                            <span class="repo-lang-legend" style="background-color:${repo.primaryLanguage.color}"></span>
+                                            <span>${repo.primaryLanguage.name}</span>
+                                        </span>` : '' }
+                
                 ${repo.stargazerCount? `<span class="repo-star meta">
                 <svg aria-label="star" class="profile-icons" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
                 ${repo.stargazerCount}
@@ -228,8 +230,6 @@ const renderUserInfo = user => {
     let menuStatus = user.status.emojiHTML
     menuStatus += `<p>${user.status.message}</p>`
     userMenu.innerHTML =  menuStatus
-    
-
 }
 
 const fetchGithubData = async () => {
@@ -252,27 +252,5 @@ const fetchGithubData = async () => {
     renderUserInfo(userDetails)
     renderRepos(repos)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener('load', fetchGithubData)
